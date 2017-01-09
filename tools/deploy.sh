4314981fc9ea1c05b298812bb5ece1bfc3c7c802
David Bruant <bruant.d@gmail.com>
2017-01-09 19:15:27 +0100

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "Deploy scriiiiipt"
+git config user.name "David Bruant (via Travis CI)"
+git config user.email "bruant.d+travisci@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git fetch upstream && git reset upstream/$DEPLOY_TARGET_BRANCH
+
+git add -A .
+git commit -m "rebuild of repo at commit https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_COMMIT"
+git push -q upstream HEAD:$DEPLOY_TARGET_BRANCH
+
